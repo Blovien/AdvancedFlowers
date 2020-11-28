6b5d74e13e6754d845a79edff92aa1792f7ef06c
Blovien <rossidario07@gmail.com>
2020-11-28 13:36:20 +0100

    Initial commit

---
+++ src/main/java/com/blovien/advancedflowers/Advancedflowers.java
@@ -0,0 +1,16 @@
+package com.blovien.advancedflowers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Advancedflowers extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
